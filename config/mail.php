bd38fb42a2ce72d6507a1d15efecc3b9b2278d6a
adama <adam.abdelmoumni@gmail.com>
2020-03-21 11:19:45 +0100

    update mail.php file with mailgun details

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'welcome@cleanupcar.com'),
+        'address' => env('MAIL_FROM_ADDRESS'),
         'name' => env('MAIL_FROM_NAME', config('app.name')),
     ],
 
